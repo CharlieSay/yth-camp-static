d976085947f53a8e2d1c8d19a2a5a1e1f579431d
charlie.say@itv.com <charliewsay@gmail.com>
2022-03-25 17:58:09 +0000

    Yth Camp Static

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main className="middle">
+        <h1 style={{ fontSize: '140px', margin: '0' }}>🏕️</h1>
+        <p style={{ color: 'white' }}>28TH-30TH JULY</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
