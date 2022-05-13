5a73110dd017ed3733ff06915c3999b82f7a390b
charsay <charlie.say@itv.com>
2022-05-13 16:47:28 +0100

    Added new branding to static camp page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import './App.css'
+import "./App.css";
+import DistortedLogo from "./audacious-logo-d.png";
+import CampLogo from "./camp-logo.png";
+import Dates from "./dates.png";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <main className="middle">
-        <h1 style={{ fontSize: '140px', margin: '0' }}>🏕️</h1>
-        <p style={{ color: 'white' }}>28TH-30TH JULY</p>
-        <a
-          style={{
-            color: 'white',
-            fontSize: '20px',
-            fontWeight: '900',
-            textDecoration: 'none',
-          }}
-          href="https://audacious.churchsuite.com/events/7cqdfbmt"
-        >
-          BOOK IN
-        </a>
-      </main>
-    </div>
-  )
+    <main className="middle">
+      <section className="img-containers">
+        <RoughNotationGroup show={true}>
+          <RoughNotation type="circle" order={1}>
+            <img
+              className="audacious-logo"
+              alt="Audacious"
+              src={DistortedLogo}
+            />
+          </RoughNotation>
+          <RoughNotation type="box" order={2}>
+            <img className="audacious-logo" alt="Youth camp" src={CampLogo} />
+          </RoughNotation>
+          <RoughNotation type="circle" order={3}>
+            <img className="audacious-logo" alt="28-30th July" src={Dates} />
+          </RoughNotation>
+          <RoughNotation type="highlight" order={4}>
+            <a
+              className="book-in-text"
+              href="https://audacious.churchsuite.com/events/7cqdfbmt"
+            >
+              BOOK IN
+            </a>
+          </RoughNotation>
+        </RoughNotationGroup>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
